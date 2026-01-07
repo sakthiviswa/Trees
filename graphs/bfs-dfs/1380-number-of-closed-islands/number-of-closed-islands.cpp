90e16514555071f12e6a244893e6588d7622712f
sakthiviswa <sakthinathan3110@gmail.com>
2026-01-08 00:17:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (58.91%) - LeetSync

--- graphs/bfs-dfs/1380-number-of-closed-islands/number-of-closed-islands.cpp
+++ graphs/bfs-dfs/1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
 
-    
-    bool dfs(vector<vector<int>>& grid,int row,int col){
+    bool dfs(vector<vector<int>> &grid,int row,int col){
 
-        int m=grid.size();
-        int n=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        if(row < 0 || row >= m || col<0 || col >= n ){
+        if(row < 0 || row >= m || col < 0 || col >= n){
             return false;
         }
 
-        if(grid[row][col] != 0){
-            return true;
+        if(grid[row][col] == 1){
+           return true;
         }
 
-        grid[row][col] = 2;
+        grid[row][col] = 1;
 
         bool up = dfs(grid,row+1,col);
-        bool down =  dfs(grid,row-1,col);
-        bool right =  dfs(grid,row,col+1);
-        bool left =  dfs(grid,row,col-1);
+        bool down = dfs(grid,row-1,col);
+        bool left = dfs(grid,row,col+1);
+        bool right = dfs(grid,row,col-1);
 
-        return up && down && right && left;
-            
+        return up && down && left && right;
+
 
 
     }
     int closedIsland(vector<vector<int>>& grid) {
 
-        int m=grid.size();
-        int n=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        int res=0;
-    
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(grid[i][j] == 0){
+        int res = 0;
 
-                if(dfs(grid,i,j)){
-                    res ++;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(grid[i][j] == 0){
+                    if(dfs(grid,i,j)){
+                        res++;
+                    }
                 }
-              
             }
-            
         }
-       } 
 
-       return res;
+        return res;
+        
     }
 };
