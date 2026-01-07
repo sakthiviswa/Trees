48b0911c3173d5948e578410a8d4f1246f6998f9
sakthiviswa <sakthinathan3110@gmail.com>
2026-01-07 21:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (99.66%) - LeetSync

---
+++ graphs/bfs-dfs/1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    
+    bool dfs(vector<vector<int>>& grid,int row,int col){
+
+        int m=grid.size();
+        int n=grid[0].size();
+
+        if(row < 0 || row >= m || col<0 || col >= n ){
+            return false;
+        }
+
+        if(grid[row][col] == 1){
+            return true;
+        }
+
+        grid[row][col] = 1;
+
+        bool up = dfs(grid,row+1,col);
+        bool down =  dfs(grid,row-1,col);
+        bool right =  dfs(grid,row,col+1);
+        bool left =  dfs(grid,row,col-1);
+
+        return up && down && right && left;
+            
+
+
+    }
+    int closedIsland(vector<vector<int>>& grid) {
+
+        int m=grid.size();
+        int n=grid[0].size();
+
+        int res=0;
+    
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j] == 0){
+
+                if(dfs(grid,i,j)){
+                    res ++;
+                }
+              
+            }
+            
+        }
+       } 
+
+       return res;
+    }
+};
