fd569fe21a6887d8474db2ac1671fac09e7bf7b1
sakthiviswa <sakthinathan3110@gmail.com>
2026-01-13 23:18:13 +0530

    Time: 83 ms (58.79%) | Memory: 44.1 MB (90.55%) - LeetSync

--- arrays/744-network-delay-time/network-delay-time.cpp
+++ arrays/744-network-delay-time/network-delay-time.cpp
@@ -2,53 +2,52 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
 
-        vector<vector<pair<int,int>>>adj(n+1);
+        vector<vector<pair<int,int>>> adj(n + 1);
 
         for(auto& i:times){
             adj[i[0]].push_back({i[1],i[2]});
         }
-        
+
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
 
-        vector<int>dis(n+1,INT_MAX);
-
-        dis[k] = 0;
 
         pq.push({0,k});
 
-        while(!pq.empty()){
+        vector<int>dis(n+1,INT_MAX);
+        dis[k] = 0;
 
+        while(!pq.empty()){
             int curdis = pq.top().first;
             int node = pq.top().second;
 
             pq.pop();
 
-            if(curdis > dis[node]){
 
+            if(curdis > dis[node]){
                 continue;
             }
 
-            for(auto& [v,w] : adj[node]){
+            for(auto& [nei,w]:adj[node]){
 
-                if(curdis + w < dis[v]){
-                    dis[v] = curdis+w;
-                    pq.push({dis[v],v});
+                if(curdis + w  < dis[nei]){
+                    dis[nei] = curdis+w;
+                    pq.push({dis[nei],nei});
                 }
             }
-
-
-
         }
+
 
         int ans = 0;
 
         for(int i=1;i<=n;i++){
+
             if(dis[i] == INT_MAX){
                 return -1;
             }
+
             ans = max(ans,dis[i]);
         }
-
+        
         return ans;
     }
 };
