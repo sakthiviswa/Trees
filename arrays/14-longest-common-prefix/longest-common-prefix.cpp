44ac7da12fa6ea7744ecff8dd70b60dc1472c4c9
sakthiviswa <sakthinathan3110@gmail.com>
2026-01-15 23:50:08 +0530

    Time: 3 ms (11.20%) | Memory: 12 MB (29.83%) - LeetSync

---
+++ arrays/14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        int n = strs.size();
+
+        sort(strs.begin(),strs.end());
+
+        string first = strs[0];
+        string last = strs[n-1];
+
+        string res ;
+
+        for(int i=0;i<min(first.size(),last.size());i++){
+
+            if(first[i] == last[i]){
+                res += last[i];
+            }
+            else
+            {
+                return res;
+            }
+        }
+
+        return res;
+    }
+};
