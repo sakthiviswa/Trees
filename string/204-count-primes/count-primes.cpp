e8c2cc9856cb01f1205bf12e07e0e7581496ec76
sakthiviswa <sakthinathan3110@gmail.com>
2026-02-06 23:30:12 +0530

    Time: 167 ms (70.14%) | Memory: 12.5 MB (90.42%) - LeetSync

--- string/204-count-primes/count-primes.cpp
+++ string/204-count-primes/count-primes.cpp
@@ -11,15 +11,17 @@ public:
         isPrime[0] = isPrime[1] = false;
 
 
-       for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
+       for(int i = 2;i * i < n; i++){
+
+            if(isPrime[i]){
+                for(int j = i*i;j < n;j += i){
                     isPrime[j] = false;
                 }
             }
-        }
+       } 
 
-        int count = 0;
+       int count = 0;
+
        for(int i = 2;i < n;i++){
             if(isPrime[i]){
                 count++;
