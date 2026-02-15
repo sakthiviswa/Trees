2014eb6609a195337025e3a8af7df09c4f6b33ac
sakthiviswa <sakthinathan3110@gmail.com>
2026-02-15 23:55:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (59.48%) - LeetSync

---
+++ string/67-add-binary/add-binary.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        string res = "";
+
+        int carry = 0;
+
+       int i = a.size()-1;
+       int j = b.size()-1;
+
+       while(i >=0 || j >= 0 || carry == 1){
+
+            int sum = carry;
+
+            if(i>=0){
+                sum += a[i]-'0';
+                i--;
+            }
+
+            if(j>=0){
+                sum += b[j]-'0';
+                j--;
+            }
+
+           res.push_back((sum % 2)+'0');
+            carry = sum / 2;
+       }
+
+       reverse(res.begin(), res.end());
+
+       return res;
+
+
+
+
+        
+    }
+};
