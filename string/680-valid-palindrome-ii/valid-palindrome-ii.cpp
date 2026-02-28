236134661c9a6dd3f266bcbdd2b190b242911423
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-02-28 22:00:27 +0530

    Time: 4 ms (59.33%) | Memory: 22 MB (94.10%) - LeetSync

---
+++ string/680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    bool ispalindrome(string &s,int left,int right){
+
+
+        while(left < right){
+
+            if(s[left] != s[right]){
+                return false;
+            }
+                left++;
+                right--;
+        }
+
+        return true;
+    }
+    bool validPalindrome(string s) {
+
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left < right){
+
+            if(s[left] == s[right]){
+                left++;
+                right--;
+            }
+            else
+            {
+                return ispalindrome(s,left+1,right) ||
+                       ispalindrome(s,left,right-1);
+            }
+        }
+
+        return true;
+        
+    }
+};
