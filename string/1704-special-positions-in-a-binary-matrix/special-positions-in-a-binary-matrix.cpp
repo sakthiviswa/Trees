7247ca720f76d0346a1ac6798513ad5efc9d3e0e
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-03-04 23:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (20.57%) - LeetSync

--- string/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ string/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
 
-int check(vector<vector<int>>& mat,int row,int col){
+int numSpecial(vector<vector<int>>& mat) {
 
         int m = mat.size();
         int n = mat[0].size();
 
-        for(int i=0;i<m;i++){
-            if(i != row && mat[i][col] == 1){
-                return 0;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if( i != col && mat[row][i] == 1){
-                return 0;
-            }
-        }
-
-        return 1;
-
-}
-    int numSpecial(vector<vector<int>>& mat) {
-
-        int m = mat.size();
-        int n = mat[0].size();
-
-        int count = 0;
+        vector<int> rowSum(m,0);
+        vector<int>colSum(n,0);
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
 
                 if(mat[i][j] == 1){
 
-                    count += check(mat,i,j);
+                   rowSum[i]++;
+                   colSum[j]++;
                 }
             }
         }
 
-        return count;
-         
+        
+
+        int count = 0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1){
+
+                    count++;
+                }
+            }
+        }
+        return count;         
     }
 };
