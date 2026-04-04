aac8ff7d5cedaabd83939231e8b7fb1ad1d4f5e5
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-04-04 15:44:09 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (27.02%) - LeetSync

---
+++ string/777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int temp = matrix[0][0];
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+
+                
+                if( matrix[i][j] != matrix[i-1][j-1]){
+                    return false;
+                }
+
+            }
+        }
+        
+        return true;
+    }
+};
