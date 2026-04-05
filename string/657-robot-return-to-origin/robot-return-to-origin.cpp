898c50f1e2e78070d05e84542e3fa7a2c0d69545
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-04-05 19:30:55 +0530

    Time: 3 ms (46.40%) | Memory: 10.3 MB (47.26%) - LeetSync

---
+++ string/657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+
+        int x = 0,y = 0;
+
+        for(char c:moves)
+        {
+            if(c == 'U'){
+                x++;
+            }
+            else if(c == 'D'){
+                x--;
+            }
+            else if(c == 'R')
+            {
+                y++;
+            }
+            else if(c == 'L'){
+                y--;
+            }
+        }
+
+
+        return x == 0 && y == 0;
+        
+    }
+};
