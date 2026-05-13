beedfa73517cf4df85a322286fba2d56bf6843b7
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-05-13 23:10:21 +0530

    Time: 2 ms (70.78%) | Memory: 10.9 MB (39.39%) - LeetSync

--- substring/424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ substring/424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -6,20 +6,19 @@ public:
         int max_len = 0;
         int max_freq = 0;
 
-        vector<int>freq(26,0);
+        vector<int> freq(26, 0);
 
         int left = 0;
-        for(int right = 0;right<n;right++){
-            freq[s[right]-'A']++;
+        for (int right = 0; right < n; right++) {
+            freq[s[right] - 'A']++;
 
-            max_freq = max(max_freq,freq[s[right]-'A']);
+            max_freq = max(max_freq, freq[s[right] - 'A']);
 
-            while((right - left +1) - max_freq> k){
-                freq[s[left]-'A']--;
+            while ((right - left + 1) - max_freq > k) {
+                freq[s[left] - 'A']--;
                 left++;
             }
-            max_len = max(max_len,(right-left+1));
-
+            max_len = max(max_len, (right - left + 1));
         }
 
         return max_len;
